6e1a84fc8f461f1113ddd9d2a86988e5b9530769
Dev Ttangkong <ttankkeo112@gmail.com>
2024-12-21 07:29:27 +0900

    commit for backup about /video post api

--- src/api/video.ts
+++ src/api/video.ts
@@ -1,20 +1,37 @@
-import { HTTPHandler } from "core";
+import { HTTPHandler, UUID } from "core";
 import { Readable } from "stream";
-import * as ffmpeg from "fluent-ffmpeg";
-
-function bufferToStream(buffer: Buffer) {
-    const stream = new Readable();
-    stream.push(buffer);
-    stream.push(null); // Defines end of stream.
-    return stream;
-}
+import fs from "fs";
+import ffmpeg from "fluent-ffmpeg";
 
 export const VIDEO_HTTP_HANDLER = new HTTPHandler({
     post: async (request, response, buffer) => {
-        const command = new ffmpeg.FfmpegCommand({source: bufferToStream(buffer)});
-        command.outputFormat("");
-        command.on("end", (out, err) => {
-            
+        // Make directory for saving temp video files.
+        if (fs.existsSync("db/videos")) {
+            fs.mkdirSync("db/videos", {recursive: true});
+        }
+
+        const uuid = UUID.v4();
+        const command = ffmpeg();
+        command.input(Readable.from(buffer));
+        command.inputFormat("mp4");
+        command.output(`db/videos/${uuid}.webm`);
+        command.outputFormat("webm");
+        command.on("start", () => {
+            console.log("Video Convertion Start");
         });
+
+        command.on("progress", (progress) => {
+            console.log(progress);
+        });
+
+        command.on("end", (out, err) => {
+            console.log(out);
+            console.log(err);
+        });
+
+        command.run();
+
+        response.writeHead(200);
+        response.end(uuid);
     }
 });
